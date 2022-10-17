7f9bf20166cc6d2f333e9881213285cf58597d0d
Magzhan <sunmorring@gmail.com>
2022-10-17 16:56:04 +0600

    движение фигур функция canMove

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -12,7 +12,11 @@ const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
   const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
 
   function onClick(cell: Cell) {
-    if (cell.figure) {
+    if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+      selectedCell.moveFigure(cell);
+      setSelectedCell(null);
+      updateBoard();
+    } else {
       setSelectedCell(cell);
     }
   }
